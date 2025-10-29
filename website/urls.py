e626e6292a445138906f26ee70315bc46863c9f6
IsaacSemb <isaacsemb1996@gmail.com>
2025-10-29 12:36:29 +0000

    adding an app name for proper scoping

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from . import views
 
+
+# this avoid scoping issues if 2 apps have the same template name
+app_name = 'website'
+
 urlpatterns = [
     path("index", view=views.index, name="home"),
     path("about", view=views.about, name="about"),
