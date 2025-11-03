7600680b29f195cd0ed59817a9f2d9ad956591b6
IsaacSemb <isaacsemb1996@gmail.com>
2025-11-03 16:59:23 +0000

    create 2 view functions one for viewing json of all todos and one for html rendering the todos with a template

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Todo
 
 # Create your views here.
+def todo_list_json(request):
+    todos = Todo.objects.all()
+    data = [
+        {
+            "id": t.id, 
+            "title": t.title,
+            "description": t.description,
+            "status": t.status
+        } for t in todos
+    ]
+    return JsonResponse(data, safe=False)
+
+def todo_list_html(request):
+    
+    todos = Todo.objects.all()
+
+    data = [
+        {
+            'id':todo.id,
+            'title':todo.title,
+            'description':todo.description,
+            'status':todo.status,
+        } for todo in todos
+    ]
+
+    return render(request, 'todos/todos.html', {'todos': data})
+
