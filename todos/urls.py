bc29e01b87fc66d5cbfc177e2fa83d406111a5ec
IsaacSemb <isaacsemb1996@gmail.com>
2025-11-03 17:00:38 +0000

    register the urls for the different views in the urls for the applcations and then into the central urls for the entire system

---
+++ todos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+
+# this avoid scoping issues if 2 apps have the same template name
+app_name = 'todos'
+
+urlpatterns = [
+    path('', views.todo_list_html, name='todo_list_html'),
+    path('list_json/', views.todo_list_json, name='todo_list_json'),
+]
