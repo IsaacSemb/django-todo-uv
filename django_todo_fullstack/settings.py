107d3cb032360656f245d47389bb5922bb17da5a
IsaacSemb <isaacsemb1996@gmail.com>
2025-11-26 01:47:41 +0000

    change db name to app.db

--- django_todo_fullstack/settings.py
+++ django_todo_fullstack/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'django_todo_fullstack.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'app.db',
     }
 }
 
